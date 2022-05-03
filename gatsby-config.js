bdea780228ae59c80492358c33535fb1bc4b7a2f
Karsten Taylor <karstentaylor0@gmail.com>
2022-05-02 19:14:59 -0700

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `My First Gatsby Site`,
+    title: `Wolves`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: 'blog',
+        path: `${__dirname}/blog`,
+      }
+    }
   ],
 }
